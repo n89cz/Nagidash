7d15cbe9abbc9b12ecf346b45bec594f338af3b5
root <n89cz>
2019-02-13 11:27:04 +0100

    php..

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 //TODO: Move all configuration items into config.php and require the file:
 //require_once 'config.php';
 
-$datasource = fopen("/var/spool/nagios/status.dat", "r") or exit("I am not able to open status.dat file. Is the path correct?");
+$data_source = fopen("/var/spool/nagios/status.dat", "r") or exit("I am not able to open status.dat file. Is the path correct?");
 $page_title = "Nagidash";
 $current_date = date('Y-m-d H:i:s');
-$refresh_rate = 30;
+$refresh_rate = 360;
 ?>
 
 <!DOCTYPE html>
@@ -28,6 +28,69 @@ $refresh_rate = 30;
 </head>
 
 <body>
+<?php
+
+//arrays for different fields in nagios status.dat
+$service_array = array();
+$host_array = array();
+$state_array = array();
+
+$critical_array = array();
+
+//loop counts
+$servicecount = 0;
+$host_count = 0;
+$curr_state_count = 0;
+
+//we want to look for these items in status.dat
+$hostname = 'host_name=';
+$service_des = 'service_description=';
+$curr_state = 'current_state=';
+$plugin_out = 'plugin_output=';
+$last_check = 'last_check=';
+
+while(!feof($data_source)){ //begin while through nagios status.dat
+    $line = fgets($data_source);
+
+    $servicepos = strpos($line,$service_des);
+    $hostpos = strpos($line,$hostname);
+
+    if ($servicepos!==false){
+	 $servicecount++;
+         $service_array[$servicecount]=substr($line,strpos($line,'=')+1,strlen($line));;
+         $check=1;
+    }
+    $check=0;
+
+    if ($hostpos!==false){
+	$host_count++;
+	$host_array[$host_count]=substr($line,strpos($line,'=')+1,strlen($line));;
+	$check=1;
+    }
+    $check=0;
+
+    if ($currpos!==false){
+	$curr_state_count++;
+	$statearray[$curr_state_count]=substr($line,strpos($line,'=')+1,strlen($line));
+	$check=1;
+    }
+    $check=0;
+}
+
+
+echo '<pre>'; print_r($service_array); echo '</pre>';
+echo '<pre>'; print_r($host_array); echo '</pre>';
+
+fclose($data_source);
+?>
+
+
+
+
+
+
+
+
 
     <div class="container-fluid">
 	<h6><?php echo($page_title); echo(" "); echo($current_date); ?></h6>
